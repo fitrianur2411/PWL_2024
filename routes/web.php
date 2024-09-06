7d17ca9f208f7f1e3e66a8ba2f568bc9ffe587f3
fitrianur2411 <fn394585@gmail.com>
2024-09-06 10:39:16 +0700

    JS2 - Praktikum 1

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\PhotoController; 
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +22,50 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Praktikum1
+//Langkah 2
+Route::get('/hello', function () { 
+return 'Hello World'; 
+}); 
+
+//Langkah 4
+Route::get('/world', function () { 
+    return 'World'; 
+    });
+
+//Langkah 6
+Route::get('/', function () { 
+    return 'Selamat Datang!'; 
+}); 
+
+//Langkah 7
+Route::get('/about', function () { 
+    return 'NIM 2241760004 adalah Fitria Nur Sholikah'; 
+}); 
+
+//Langkah 8
+Route::get('/user/{name}', function ($name) { 
+    return 'Nama saya '.$name; 
+});
+
+//Langkah 11
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) { 
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId; 
+}); 
+
+//Langkah 13
+Route::get('/articles/{id}', function ($Id) { 
+    return 'Halaman Artikel dengan ID : '.$Id; 
+}); 
+
+//Langkah 14
+Route::get('/user/{name?}', function ($name=null) { 
+    return 'Nama saya '.$name; 
+}); 
+
+//Langkah 17
+Route::get('/user/{name?}', function ($name='John') { 
+    return 'Nama saya '.$name; 
+});
+
